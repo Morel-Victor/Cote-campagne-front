f9a3123a3c4595c65668010ef17086816190d7a0
Morel Victor <morel.victor1@gmail.com>
2022-02-02 15:44:48 +0100

    Add two views

---
+++ src/views/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <Gallerys />
+  </div>
+</template>
+
+<script>
+import Gallerys from "./components/Gallerys.vue";
+
+export default {
+  name: "Gallery",
+  components: {
+    Gallerys,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style></style>
