7ae735e3e739e584e13a785d484374c856551f97
Morel Victor <morel.victor1@gmail.com>
2022-02-02 17:10:58 +0100

    Add carousel in home

---
+++ src/components/Carousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <carousel :items-to-show="1">
+      <slide v-for="slide in 3" :key="slide">
+        {{ slide }}
+      </slide>
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+
+export default {
+  name: "App",
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+};
+</script>
+
+<style>
+.container {
+  width: 80vw;
+  display: flex;
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
