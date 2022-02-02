ec4819485a57e94e7a442cb14bafeffa628c50b3
Morel Victor <morel.victor1@gmail.com>
2022-02-02 16:41:05 +0100

    Add router in main

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from "./App.vue";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
+import router from './router'
 
-const app = createApp(App);
+const app = createApp(App).use(router);
 
 app.use(AOS.init());
 
