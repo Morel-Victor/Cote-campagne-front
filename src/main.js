c6514d81a6ce0deee14be6c834940c1ddcf510ec
Morel Victor <morel.victor1@gmail.com>
2022-02-02 15:43:10 +0100

    init aos on global

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const app = createApp(App);
+
+app.use(AOS.init());
+
+app.mount("#app");
