cf3be0f5b2eb07c520b6e40343cf2c1ecfb7952a
Morel Victor <morel.victor1@gmail.com>
2022-02-04 14:23:03 +0100

    Add footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="col1">
+      <button type="button" class="socialBtn">
+        <img class="socialBtn instagram" src="@/assets/instagram.png" />
+      </button>
+      <button type="button" class="socialBtn">
+        <img class="socialBtn facebook" src="@/assets/facebook.png" />
+      </button>
+    </div>
+    <div class="col2">
+      <p>côté campagne sarl</p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style src="./style.css" scoped></style>
