aed28a6c376bc714772bd29b21bd44c143fea84e
Morel Victor <morel.victor1@gmail.com>
2022-02-15 19:59:06 +0100

    Add CardGallery

---
+++ src/components/GalleryC/CardGallery/CardGallery.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Card">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardGallery",
+  // props: {
+  //   msg: String,
+  // }, {{ msg }}
+};
+</script>
+
+<style src="./style.css" scoped></style>
