c0487e23e1db620b9c8c8529a514d913d2cef446
Morel Victor <morel.victor1@gmail.com>
2022-02-15 19:09:23 +0100

    Add 404 page

---
+++ src/components/NotFound/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+      <p>404 NOT FOUND</p>
+    <img src="@/assets/Flower.png" class="divider" alt="fleur"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  // props: {
+  //   msg: String,
+  // }, {{ msg }}
+};
+</script>
+
+<style src="./style.css" scoped></style>
+
+
